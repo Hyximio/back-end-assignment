9f153cebc86e93cf37bdb7d40fbd78418e155cee
Mick Munsterman <mickmunsterman@gmail.com>
2022-10-26 13:53:09 +0200

    setup

---
+++ src/main/java/com/mmbackendassignment/mmbackendassignment/MmBackEndAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.mmbackendassignment.mmbackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmBackEndAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmBackEndAssignmentApplication.class, args);
+	}
+
+}
